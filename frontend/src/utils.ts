64469ac019118bcd6e9b91727c291ec6226d4795
shekohex <dev+github@shadykhalifa.me>
2024-03-06 21:58:05 +0000

    Add currency mapping for unsupported currencies

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,4 +1,18 @@
 export function getCurrencyFormatter(currency: string) {
+    // Handles the case where the currency is not supported by the Intl API
+    // like stablecoins.
+    const CURRENCY_MAPPER = {
+        USDT: "USD",
+        USDC: "USD",
+        TUSD: "USD",
+        BUSD: "USD",
+        FDUSD: "USD",
+        DAI: "USD",
+        PYUSD: "USD",
+        EURC: "EUR",
+        EURT: "EUR",
+    };
+    currency = CURRENCY_MAPPER[currency] || currency;
     const currencyFormat = new Intl.NumberFormat(undefined, {
         style: "currency",
         currency,
