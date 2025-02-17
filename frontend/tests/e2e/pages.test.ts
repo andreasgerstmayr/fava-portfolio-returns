353e87bd96013728c00e1991149277849dce38bc
Thomas den Hollander <denhollander.thomas@gmail.com>
2025-02-17 23:30:35 +0100

    Test dark mode

--- frontend/tests/e2e/pages.test.ts
+++ frontend/tests/e2e/pages.test.ts
@@ -3,16 +3,26 @@ import "jest-puppeteer";
 
 const BASE_URL = "http://127.0.0.1:5000/beancount/extension/FavaPortfolioReturns/";
 const tests = [
-  { name: "Portfolio", url: "" },
-  { name: "Performance", url: "#/performance?compareWith=c:VHT_c:GLD" },
-  { name: "Performance (TWR)", url: "#/performance?investments=g:Gold&method=twr&compareWith=c:GLD" },
-  { name: "Returns", url: "#/returns" },
-  { name: "Returns (MDM)", url: "#/returns?method=mdm" },
-  { name: "Returns (TWR)", url: "#/returns?method=twr" },
-  { name: "Dividends", url: "#/dividends" },
-  { name: "Cash Flows", url: "#/cash_flows?investments=c:VHT" },
-  { name: "Groups", url: "#/groups" },
-  { name: "Investments", url: "#/investments" },
+  { name: "Portfolio", mode: "light", url: "" },
+  { name: "Performance", mode: "light", url: "#/performance?compareWith=c:VHT_c:GLD" },
+  { name: "Performance (TWR)", mode: "light", url: "#/performance?investments=g:Gold&method=twr&compareWith=c:GLD" },
+  { name: "Returns", mode: "light", url: "#/returns" },
+  { name: "Returns (MDM)", mode: "light", url: "#/returns?method=mdm" },
+  { name: "Returns (TWR)", mode: "light", url: "#/returns?method=twr" },
+  { name: "Dividends", mode: "light", url: "#/dividends" },
+  { name: "Cash Flows", mode: "light", url: "#/cash_flows?investments=c:VHT" },
+  { name: "Groups", mode: "light", url: "#/groups" },
+  { name: "Investments", mode: "light", url: "#/investments" },
+  { name: "Portfolio (dark)", mode: "dark", url: "" },
+  { name: "Performance (dark)", mode: "dark", url: "#/performance?compareWith=c:VHT_c:GLD" },
+  { name: "Performance (TWR) (dark)", mode: "dark", url: "#/performance?investments=g:Gold&method=twr&compareWith=c:GLD" },
+  { name: "Returns (dark)", mode: "dark", url: "#/returns" },
+  { name: "Returns (MDM) (dark)", mode: "dark", url: "#/returns?method=mdm" },
+  { name: "Returns (TWR) (dark)", mode: "dark", url: "#/returns?method=twr" },
+  { name: "Dividends (dark)", mode: "dark", url: "#/dividends" },
+  { name: "Cash Flows (dark)", mode: "dark", url: "#/cash_flows?investments=c:VHT" },
+  { name: "Groups (dark)", mode: "dark", url: "#/groups" },
+  { name: "Investments (dark)", mode: "dark", url: "#/investments" },
 ];
 
 function customSnapshotIdentifier(p: { currentTestName: string }) {
@@ -30,7 +40,10 @@ describe("PNG Snapshot Tests", () => {
     await page.setUserAgent("puppeteer-png");
   });
 
-  it.each(tests)("$name", async ({ url }) => {
+  it.each(tests)("$name", async ({ mode, url }) => {
+    await page.emulateMediaFeatures([
+      { name: "prefers-color-scheme", value: mode },
+    ]);
     await page.goto(`${BASE_URL}${url}`);
     await page.evaluate(() => {
       // full page screenshot doesn't work due to sticky sidebar
@@ -48,7 +61,10 @@ describe("HTML Snapshot Tests", () => {
     await page.setUserAgent("puppeteer-html");
   });
 
-  it.each(tests)("$name", async ({ url }) => {
+  it.each(tests)("$name", async ({ url, mode }) => {
+    await page.emulateMediaFeatures([
+      { name: "prefers-color-scheme", value: mode },
+    ]);
     await page.goto(`${BASE_URL}${url}`);
     await page.waitForNetworkIdle();
 
