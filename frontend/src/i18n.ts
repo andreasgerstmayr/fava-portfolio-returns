d692e0041ac424b8bdbcf751ee8e4beccbdaf790
Andreas Gerstmayr <andreas@gerstmayr.me>
2026-01-25 23:46:33 +0100

    chore: treat empty translation values as missing

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -6,9 +6,13 @@ i18n.use(initReactI18next).init({
   resources: locales,
   lng: "en",
   fallbackLng: "en",
+  // The extraction adds empty strings for untranslated translation keys, these should be considered as missing translations
+  returnEmptyString: false,
+  // do not use key and namespace separators in translation keys
   keySeparator: false,
   nsSeparator: false,
   interpolation: {
-    escapeValue: false, // React already escapes values to protect against XSS
+    // React already escapes values to protect against XSS
+    escapeValue: false,
   },
 });
